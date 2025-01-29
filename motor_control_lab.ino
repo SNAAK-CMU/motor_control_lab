ae3fdab9ed81905f567e3ef329113d7a821a4578
RodrigoCatto <rodrigo.catto7@gmail.com>
2025-01-29 17:23:51 -0500

    Added potentiometer and servo

--- motor_control_lab.ino
+++ motor_control_lab.ino
@@ -1,11 +1,20 @@
 #include "ultrasonic.h"
+#include "potentiometer.h"
+#include "servo.h"
 
-// void setup() {
-//   Serial.begin(9600);
-//   setup_ultrasonic(9, 8);
-// }
+#define ultrassonic_pin A1
 
-// void loop() {
-//   long cm = read_ultrasonic(9, 8);
-//   Serial.println(cm);
-// }
+void setup() {
+  Serial.begin(9600);
+  // setup_ultrasonic(9, 8);
+  // setup_potentiometer(ultrassonic_pin);
+  // setup_servo(2);
+}
+
+void loop() {
+  // long cm = read_ultrasonic(9, 8);
+  // float val = read_potentiometer(ultrassonic_pin);
+  // set_servo_angle(90);
+
+  // Serial.println(val);
+}

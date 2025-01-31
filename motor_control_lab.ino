e0cc58c32c8f5676d13bc74ec44ef43123d92779
oberton31 <bertonoliver50@gmail.com>
2025-01-31 18:03:49 -0500

    adding slot sensor

--- motor_control_lab.ino
+++ motor_control_lab.ino
@@ -1,20 +1,25 @@
 #include "ultrasonic.h"
 #include "potentiometer.h"
 #include "servo.h"
+#include "slot.h"
 
-#define ultrassonic_pin A1
-
+#define ULTRASONIC_PIN A1
+#define SLOT_PIN A5
 void setup() {
   Serial.begin(9600);
   // setup_ultrasonic(9, 8);
-  // setup_potentiometer(ultrassonic_pin);
+  // setup_potentiometer(ULTRASONIC_PIN);
   // setup_servo(2);
+  setup_slot(SLOT_PIN);
 }
 
 void loop() {
   // long cm = read_ultrasonic(9, 8);
-  // float val = read_potentiometer(ultrassonic_pin);
+  // float val = read_potentiometer(ULTRASONIC_PIN);
   // set_servo_angle(90);
 
   // Serial.println(val);
+  bool output = read_slot(SLOT_PIN);
+  Serial.println(output);
+  delay(1);
 }
